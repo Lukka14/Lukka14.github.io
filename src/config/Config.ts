ff815d3d42aa680e0b13bab58499983a8939dea6
Lukka14 <cxomelidze2003@gmail.com>
2024-11-26 01:37:18 +0100

    fixing config.ts

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,20 +1,21 @@
 // const API_URL = "http://localhost:8080";
 const API_URL = "https://api.movieplus.live";
 
-const URL_SEARCH = API_URL+"/search";
+const URL_SEARCH = `${API_URL}/search`;
 
-export enum Endpoints{
-    MOVIES = `${URL_SEARCH}/movies`,
-    SERIES = `${URL_SEARCH}/series`,
-    MULTI = `${URL_SEARCH}/multi`,
+// Use an object literal for endpoints
+export const Endpoints = {
+  MOVIES: `${URL_SEARCH}/movies`,
+  SERIES: `${URL_SEARCH}/series`,
+  MULTI: `${URL_SEARCH}/multi`,
 
-    DETAILED_MOVIE = `${URL_SEARCH}/movieDetails`,
-    DETAILED_SERIES = `${URL_SEARCH}/seriesDetails`,
-  };
+  DETAILED_MOVIE: `${URL_SEARCH}/movieDetails`,
+  DETAILED_SERIES: `${URL_SEARCH}/seriesDetails`,
+};
 
-
+// Use an enum for static, string-literal route paths
 export enum RoutePaths {
-    HOME = '/',
-    WATCH = '/watch',
-    LOGIN = '/login',
+  HOME = '/',
+  WATCH = '/watch',
+  LOGIN = '/login',
 }
