1aa69b76c580999f5642f149e01699925a6a9ceb
Lukka14 <cxomelidze2003@gmail.com>
2024-12-08 04:00:17 +0100

    update WIP

--- src/tmp/WIP.tsx
+++ src/tmp/WIP.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const WIP: React.FC = () => {
     return (
-        <div style={{ textAlign: 'center', marginTop: '20%',color: 'white' }}>
+        <div style={{ textAlign: 'center', marginTop: '15%',color: 'white' }}>
             <h1>🚧 Work in Progress 🚧</h1>
             <p>I'm working to bring you this page. Stay tuned!</p>
+            <p>But you still can watch the movies right now!</p>
+            <p>Go ahead, search something...</p>
         </div>
     );
 };
