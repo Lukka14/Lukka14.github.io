66eceb239c0b4d920a8b9ef4ee81b337236dfe59
Tsotne Pharsenadze <181521735+TsotnePharsenadze@users.noreply.github.com>
2025-05-08 02:02:49 +0400

    depl related adjustments

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
